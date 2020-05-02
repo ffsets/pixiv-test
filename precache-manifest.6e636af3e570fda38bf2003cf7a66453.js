b0a67699cb3889c0d024dd7c2ed08869fe7e9d28
ffsets <fukfuksets@gmail.com>
2020-05-01 22:23:03 -0300

    Updates

---
+++ precache-manifest.6e636af3e570fda38bf2003cf7a66453.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35aaaacc4f0900428fb702d0e1ad3eaa",
+    "url": "/pixiv-test/index.html"
+  },
+  {
+    "revision": "092cc1eb833d3825a11c",
+    "url": "/pixiv-test/static/js/2.1dc3432e.chunk.js"
+  },
+  {
+    "revision": "4f86d5dfd39c99aa6769",
+    "url": "/pixiv-test/static/js/main.bfe0ff3d.chunk.js"
+  },
+  {
+    "revision": "06a40686df1db5541273",
+    "url": "/pixiv-test/static/js/runtime~main.a1118d3e.js"
+  }
+]);
